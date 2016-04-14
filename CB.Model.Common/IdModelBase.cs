4c7e849c51593f1121392e90f541f7e8c52e5c8b
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-14 21:03:31 +0700

    Add IdModelBase

---
+++ CB.Model.Common/IdModelBase.cs
@@ -0,0 +1,26 @@
+﻿namespace CB.Model.Common
+{
+    public class IdModelBase: ObservableObject
+    {
+        #region Fields
+        private int? _id;
+        #endregion
+
+
+        #region  Properties & Indexers
+        public int? Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+        #endregion
+
+
+        #region Methods
+        public virtual void CopyFrom(IdModelBase obj, bool copyId)
+        {
+            if (copyId) Id = obj.Id;
+        }
+        #endregion
+    }
+}
