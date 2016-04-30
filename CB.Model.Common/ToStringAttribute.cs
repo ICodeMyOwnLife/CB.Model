92c56cfd563c885adf218a3f31be53b6a763b95d
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-30 21:37:29 +0700

    Edit ToStringAttribute

--- CB.Model.Common/ToStringAttribute.cs
+++ CB.Model.Common/ToStringAttribute.cs
@@ -5,6 +5,7 @@ namespace CB.Model.Common
 {
     public class ToStringAttribute: Attribute
     {
+        
         #region  Properties & Indexers
         public int OrderIndex { get; set; } = int.MaxValue;
         #endregion
