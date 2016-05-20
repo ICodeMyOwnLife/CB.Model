87357b9f8b154bb0b2d7bda3e1e766f8ff5a25e1
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-05-20 15:54:58 +0700

    Add Test.CB.Model.Serialization

---
+++ Test.CB.Model.Serialization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CB.Model.Serialization;
+
+
+namespace Test.CB.Model.Serialization
+{
+    internal class Program
+    {
+        #region Implementation
+        private static void Main()
+        {
+            var i15 = 15;
+            var s15 = new JsonModelSerializer().Serialize(i15);
+            Console.WriteLine(s15);
+
+            i15 = new JsonModelSerializer().Deserialize<int>(s15);
+            Console.WriteLine(i15);
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
