ceec6516385c8470f400c245fe114f4c5f7b33eb
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-05-29 22:09:29 +0700

    Add ConfirmationViewModelBase

---
+++ CB.Model.Prism/ConfirmationViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Interactivity.InteractionRequest;
+
+
+namespace CB.Model.Prism
+{
+    public class ConfirmationViewModelBase: PrismViewModelBase, IConfirmation
+    {
+        #region Fields
+        private bool _confirmed;
+        private object _content;
+        private string _title;
+        #endregion
+
+
+        #region  Properties & Indexers
+        public bool Confirmed
+        {
+            get { return _confirmed; }
+            set { SetProperty(ref _confirmed, value); }
+        }
+
+        public object Content
+        {
+            get { return _content; }
+            set { SetProperty(ref _content, value); }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        #endregion
+    }
+}
