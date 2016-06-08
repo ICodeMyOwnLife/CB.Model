1fd4c7a10cbff3b0e3f38f335e5114b23067374c
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-08 17:32:14 +0700

    Add EventArgs<>

---
+++ CB.Model.Common/EventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace CB.Model.Common
+{
+    public class EventArgs<T>: EventArgs
+    {
+        #region  Constructors & Destructor
+        public EventArgs(T value)
+        {
+            Value = value;
+        }
+        #endregion
+
+
+        #region  Properties & Indexers
+        public T Value { get; set; }
+        #endregion
+    }
+}
