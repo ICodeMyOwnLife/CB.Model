05f023ee9ea38df436c38be175b0681da25684a6
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-14 07:24:12 +0700

    Add PrismModelBase

---
+++ CB.Model.Prism/DomainObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Prism.Mvvm;
+
+
+namespace CB.Model.Prism
+{
+    public abstract class DomainObject: INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        #region Fields
+        private ErrorsContainer<ValidationResult> _errorsContainer;
+        #endregion
+
+
+        #region  Properties & Indexers
+        protected ErrorsContainer<ValidationResult> ErrorsContainer => _errorsContainer ?? (_errorsContainer = new ErrorsContainer<ValidationResult>(RaiseErrorsChanged));
+
+        public bool HasErrors
+            => ErrorsContainer.HasErrors;
+        #endregion
+
+
+        #region Events
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+
+        #region Methods
+        public IEnumerable GetErrors(string propertyName)
+            => _errorsContainer.GetErrors(propertyName);
+        #endregion
+
+
+        #region Implementation
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e)
+            => ErrorsChanged?.Invoke(this, e);
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected void RaiseErrorsChanged(string propertyName)
+        {
+            OnErrorsChanged(new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void ValidateProperty(string propertyName, object value)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentNullException("propertyName");
+            }
+
+            ValidateProperty(new ValidationContext(this, null, null) { MemberName = propertyName }, value);
+        }
+
+        protected virtual void ValidateProperty(ValidationContext validationContext, object value)
+        {
+            if (validationContext == null)
+            {
+                throw new ArgumentNullException("validationContext");
+            }
+
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateProperty(value, validationContext, validationResults);
+
+            ErrorsContainer.SetErrors(validationContext.MemberName, validationResults);
+        }
+        #endregion
+    }
+}
+
+
+// TODO: Delete DomainObject
---
+++ CB.Model.Prism/PrismModelBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using CB.Model.Common;
+using Prism.Mvvm;
+
+
+namespace CB.Model.Prism
+{
+    public abstract class PrismModelBase: BindableObject, INotifyDataErrorInfo
+    {
+        #region Fields
+        private ErrorsContainer<ValidationResult> _errorsContainer;
+        #endregion
+
+
+        #region  Properties & Indexers
+        protected ErrorsContainer<ValidationResult> ErrorsContainer
+            => _errorsContainer ?? (_errorsContainer = new ErrorsContainer<ValidationResult>(OnErrorsChanged));
+
+        public virtual bool HasErrors => _errorsContainer.HasErrors;
+        #endregion
+
+
+        #region Events
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        #endregion
+
+
+        #region Methods
+        public virtual IEnumerable GetErrors(string propertyName)
+            => _errorsContainer.GetErrors(propertyName);
+        #endregion
+
+
+        #region Implementation
+        protected virtual void OnErrorsChanged(string propertyName)
+            => OnErrorsChanged(new DataErrorsChangedEventArgs(propertyName));
+
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e)
+            => ErrorsChanged?.Invoke(this, e);
+
+        protected virtual void ValidateProperty(string propertyName, object value)
+        {
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+
+            ValidateProperty(new ValidationContext(this, null, null) { MemberName = propertyName }, value);
+        }
+
+        protected virtual void ValidateProperty(ValidationContext validationContext, object value)
+        {
+            if (validationContext == null) throw new ArgumentNullException(nameof(validationContext));
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateProperty(value, validationContext, validationResults);
+            ErrorsContainer.SetErrors(validationContext.MemberName, validationResults);
+        }
+        #endregion
+    }
+}
