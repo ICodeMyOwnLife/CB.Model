a0f52ab4f84f3a078324edb8cf9890df0effb118
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-28 20:58:38 +0700

    Add Disposer class

---
+++ CB.Model.Prism/Disposer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+using Prism.Commands;
+
+
+namespace CB.Model.Prism
+{
+    public class Disposer: IDisposable
+    {
+        #region Fields
+        private readonly IDisposable _disposable;
+        #endregion
+
+
+        #region  Constructors & Destructor
+        public Disposer(IDisposable disposable)
+        {
+            _disposable = disposable;
+            DisposeCommand = new DelegateCommand(Dispose);
+        }
+        #endregion
+
+
+        #region  Commands
+        public ICommand DisposeCommand { get; }
+        #endregion
+
+
+        #region Methods
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+        #endregion
+    }
+}
